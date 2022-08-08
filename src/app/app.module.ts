5d6c86a6629102312beaabe282be43545825bd74
Sai Madhuri Kasam <saimadhuri.kasam@valuemomentum.com>
2022-08-08 11:10:58 +0530

    bookapp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { ListBookComponent } from './list-book/list-book.component';
+import { ViewBookComponent } from './view-book/view-book.component';
+import { EditBookComponent } from './edit-book/edit-book.component';
+import { TitlePipe } from './title.pipe';
+import { AuthorPipe } from './author.pipe';
+import { PricePipe } from './price.pipe';
+import { YearPipe } from './year.pipe';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { ListOrdersComponent } from './list-orders/list-orders.component';
+import { LoginComponent } from './login/login.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddBookComponent,
+    ListBookComponent,
+    ViewBookComponent,
+    EditBookComponent,
+    TitlePipe,
+    AuthorPipe,
+    PricePipe,
+    YearPipe,
+    HeaderComponent,
+    ListOrdersComponent,
+    LoginComponent,
+    MyOrdersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+    
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/list-book/list-book.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-list-book',
+  templateUrl: './list-book.component.html',
+  styleUrls: ['./list-book.component.css']
+})
+export class ListBookComponent implements OnInit {
+  constructor(private http: HttpClient) { 
+    console.log("ListBookComponent constructor");
+
+  }
+
+  ngOnInit(): void {
+    console.log("Lifecycle Init method");
+    this.getBooks();
+  }
+  books!:any[];
+  title:string = "";
+
+  getBooks(){
+    const url = "http://localhost:3000/books";
+    this.http.get(url).subscribe ( (res:any)=>{
+      console.log(res);
+      this.books = res;
+    });
+  }
+
+}
